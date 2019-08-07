31c0da07e60cf82ca53cd3f537c05cb77377c741
playrey2010 <playrey2010@gmail.com>
2019-08-07 19:57:18 -0400

    app online

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -45,7 +45,7 @@ public class HomeController {
             return "messageform";
         }
         else if (file.isEmpty() && message.getPhoto().equals("")){
-            message.setPhoto(null);git
+            message.setPhoto(null);
             messageRepository.save(message);
             System.out.println("file empty and message get photo is null");
             return "redirect:/";
